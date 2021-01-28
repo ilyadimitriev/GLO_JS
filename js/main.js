e85cfc573cdd2ca9f742f4b3d96fe2465b5bcefc
ilyadimitriev <idimitriev123@gmail.com>
2021-01-28 17:41:14 +1000

    Выполнил Урок 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let money = 50;
+let income = 100;
+let addExpenses = 20;
+let deposit = 10;
+let mission = 20;
+let period = 15;
+
+alert('GitHub');
+
+console.log('Вывести в консоль сообщение с любым текстом');
